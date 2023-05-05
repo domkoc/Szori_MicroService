545cf14126d99032f8925df1bf0cf523be9186dd
Dominik Kocka <kocka98@gmail.com>
2023-05-05 18:00:41 +0200

    Add test client

---
+++ TestClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Streaming;
+
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+var client = new MovieStream.MovieStreamClient(channel);
+
+var reply = await client.GetMoviesAsync(new GetMoviesRequest());
+Console.WriteLine(reply.Movies);
+
+var reply2 = client.Watch(new WatchRequest() { StartPosition = 5, Title = "e" });
+
+var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+try
+{
+    await foreach (var watchReply in reply2.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+    {
+        Console.WriteLine($"{watchReply.Frame}");
+    }
+}
+catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+{
+    Console.WriteLine("Stream cancelled.");
+}
+Console.ReadKey();
